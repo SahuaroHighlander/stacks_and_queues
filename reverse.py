6ec96e89a3124ca84ffdbc17ee40613ab1b72608
SahuaroHighlander <168250610+SahuaroHighlander@users.noreply.github.com>
2024-05-05 18:31:39 -0600

    successfully completed the reverse.py by using the flip function written in the practice exercises

--- reverse.py
+++ reverse.py
@@ -1,18 +1,30 @@
 # MODIFY ME TO IMPLEMENT YOUR SOLUTION
 # TO PROBLEM 1: REVERSE QUEUE
 #
-# NAME:         FIXME
+# NAME:         Tara Haller
 # ASSIGNMENT:   Project 2: Stacks & Queues
 
 from Queue import Queue
 from Stack import Stack
+def flip(stack):
+    rev_stack = Stack([])
+    while stack.is_empty() == False:
+        item = stack.pop()
+        rev_stack.push(item)
+    return rev_stack
 
+flip(Stack([1,2,3,4])).print()
 
 # Return a new queue in reverse order
 # Hint: can use a stack to help
 def reverse(q_orig):
     q_new = Queue([])
-    # FIXME
+    n_stack = Stack([])
+    
+    while q_orig.is_empty() == False:
+        n_stack.push(q_orig.deq())
+        
+    q_new=flip(n_stack)
     return q_new
 
 def main():
